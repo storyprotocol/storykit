da5f436ae7b0f6ec2768c16bd43b40a197a6e2cc
samfairbairn <samfairbairn@hotmail.co.uk>
2024-04-05 10:50:23 +0800

    fix: use react_app prefix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import { QueryOptions, ResourceType } from "./types"
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL || ""
-const API_KEY = process.env.NEXT_PUBLIC_STORY_PROTOCOL_X_API_KEY || process.env.STORY_PROTOCOL_X_API_KEY || ""
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || process.env.API_BASE_URL || ""
+const API_KEY = process.env.REACT_APP_STORY_PROTOCOL_X_API_KEY || process.env.STORY_PROTOCOL_X_API_KEY || ""
 
 export async function getResource(resourceName: ResourceType, resourceId: string) {
   try {
