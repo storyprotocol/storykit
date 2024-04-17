8ae73409de141978b4a92b5731d7444ca232352e
samfairbairn <samfairbairn@hotmail.co.uk>
2024-04-17 20:49:34 +0800

    asset widget: disable policy data accordion

--- src/components/IPAssetWidget/IPAssetWidget.tsx
+++ src/components/IPAssetWidget/IPAssetWidget.tsx
@@ -1,10 +1,12 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+// import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { convertAssetToGraphFormat } from "@/lib/graph"
-import { Asset, POLICY_TYPE } from "@/lib/types"
-import { cn, getPolicyTypeByPILData, shortenAddress } from "@/lib/utils"
+// import { Asset, POLICY_TYPE } from "@/lib/types"
+import { Asset } from "@/lib/types"
+// import { cn, getPolicyTypeByPILData, shortenAddress } from "@/lib/utils"
+import { cn, shortenAddress } from "@/lib/utils"
 import { IPA_CARD_TABS, IPAssetProvider, useIPAssetContext } from "@/providers"
 import { Menu, Transition } from "@headlessui/react"
-import { Check, X } from "lucide-react"
+// import { Check, X } from "lucide-react"
 import React, { Fragment } from "react"
 import Chart from "react-apexcharts"
 import ForceGraph2D from "react-force-graph-2d"
@@ -400,84 +402,85 @@ function IPAssetOverview({ isBottomNav }: { isBottomNav?: boolean }) {
   )
 }
 
-function PILLabel({ type }: { type: string }) {
-  if (type === POLICY_TYPE.OPEN_DOMAIN) {
-    return (
-      <span className="flex flex-wrap items-center gap-1 text-xs font-light">
-        <X className="size-2" /> attribution
-        <Check className="size-2" /> commercial use
-        <Check className="size-2" /> derivatives
-        <Check className="size-2" /> all content
-      </span>
-    )
-  } else if (type === POLICY_TYPE.FREE_ATTRIBUTION) {
-    return (
-      <span className="flex flex-wrap items-center gap-1 text-xs font-light">
-        <X className="size-2" /> attribution
-        <Check className="size-2" /> commercial use
-        <Check className="size-2" /> derivatives
-        <Check className="size-2" /> all content
-      </span>
-    )
-  } else if (type === POLICY_TYPE.PAID_ATTRIBUTION) {
-    return (
-      <span className="flex flex-wrap items-center gap-1 text-xs font-light">
-        <X className="size-2" /> attribution
-        <Check className="size-2" /> commercial use
-        <Check className="size-2" /> derivatives
-        <Check className="size-2" /> all content
-      </span>
-    )
-  } else if (type === POLICY_TYPE.PAID_NO_ATTRIBUTION) {
-    return (
-      <span className="flex flex-wrap items-center gap-1 text-xs font-light">
-        <X className="size-2" /> attribution
-        <Check className="size-2" /> commercial use
-        <Check className="size-2" /> derivatives
-        <Check className="size-2" /> all content
-      </span>
-    )
-  } else if (type === POLICY_TYPE.NO_DERIVATIVE) {
-    return (
-      <span className="flex flex-wrap items-center gap-1 text-xs font-light">
-        <X className="size-2" /> attribution
-        <Check className="size-2" /> commercial use
-        <Check className="size-2" /> derivatives
-        <Check className="size-2" /> all content
-      </span>
-    )
-  }
-  return <></>
-}
+// function PILLabel({ type }: { type: string }) {
+//   if (type === POLICY_TYPE.OPEN_DOMAIN) {
+//     return (
+//       <span className="flex flex-wrap items-center gap-1 text-xs font-light">
+//         <X className="size-2" /> attribution
+//         <Check className="size-2" /> commercial use
+//         <Check className="size-2" /> derivatives
+//         <Check className="size-2" /> all content
+//       </span>
+//     )
+//   } else if (type === POLICY_TYPE.FREE_ATTRIBUTION) {
+//     return (
+//       <span className="flex flex-wrap items-center gap-1 text-xs font-light">
+//         <X className="size-2" /> attribution
+//         <Check className="size-2" /> commercial use
+//         <Check className="size-2" /> derivatives
+//         <Check className="size-2" /> all content
+//       </span>
+//     )
+//   } else if (type === POLICY_TYPE.PAID_ATTRIBUTION) {
+//     return (
+//       <span className="flex flex-wrap items-center gap-1 text-xs font-light">
+//         <X className="size-2" /> attribution
+//         <Check className="size-2" /> commercial use
+//         <Check className="size-2" /> derivatives
+//         <Check className="size-2" /> all content
+//       </span>
+//     )
+//   } else if (type === POLICY_TYPE.PAID_NO_ATTRIBUTION) {
+//     return (
+//       <span className="flex flex-wrap items-center gap-1 text-xs font-light">
+//         <X className="size-2" /> attribution
+//         <Check className="size-2" /> commercial use
+//         <Check className="size-2" /> derivatives
+//         <Check className="size-2" /> all content
+//       </span>
+//     )
+//   } else if (type === POLICY_TYPE.NO_DERIVATIVE) {
+//     return (
+//       <span className="flex flex-wrap items-center gap-1 text-xs font-light">
+//         <X className="size-2" /> attribution
+//         <Check className="size-2" /> commercial use
+//         <Check className="size-2" /> derivatives
+//         <Check className="size-2" /> all content
+//       </span>
+//     )
+//   }
+//   return <></>
+// }
 
 function IPAssetPolicy() {
   const { policyData } = useIPAssetContext()
 
   return policyData ? (
-    <Accordion type="single" collapsible className="px-2">
-      {policyData?.map((policy) => (
-        <AccordionItem key={policy.id} value={policy.id}>
-          <AccordionTrigger>
-            <div className="flex flex-col text-left">
-              <p className="text-[10px] text-slate-400">Policy ID {policy.id}</p>
-              <p className="text-xs">{getPolicyTypeByPILData(policy.pil)}</p>
-              <PILLabel type={getPolicyTypeByPILData(policy.pil)} />
-            </div>
-          </AccordionTrigger>
-          <AccordionContent>
-            <dl className="divide-y divide-gray-100 overflow-x-hidden text-sm leading-6">
-              {Object.entries(policy?.pil).map(([key, value]) => (
-                <div key={key} className="flex justify-between gap-x-4 py-1">
-                  <dt className="text-xs capitalize text-gray-500">{key}</dt>
-                  <dd className="truncate text-gray-700">{value.toString()}</dd>
-                </div>
-              ))}
-            </dl>
-          </AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
+    <div className="flex flex-col">{policyData?.map((policy) => <span key={policy.id}>id: {policy.id}</span>)}</div>
   ) : (
+    // <Accordion type="single" collapsible className="px-2">
+    //   {policyData?.map((policy) => (
+    //     <AccordionItem key={policy.id} value={policy.id}>
+    //       <AccordionTrigger>
+    //         <div className="flex flex-col text-left">
+    //           <p className="text-[10px] text-slate-400">Policy ID {policy.id}</p>
+    //           <p className="text-xs">{getPolicyTypeByPILData(policy.pil)}</p>
+    //           <PILLabel type={getPolicyTypeByPILData(policy.pil)} />
+    //         </div>
+    //       </AccordionTrigger>
+    //       <AccordionContent>
+    //         <dl className="divide-y divide-gray-100 overflow-x-hidden text-sm leading-6">
+    //           {Object.entries(policy?.pil).map(([key, value]) => (
+    //             <div key={key} className="flex justify-between gap-x-4 py-1">
+    //               <dt className="text-xs capitalize text-gray-500">{key}</dt>
+    //               <dd className="truncate text-gray-700">{value.toString()}</dd>
+    //             </div>
+    //           ))}
+    //         </dl>
+    //       </AccordionContent>
+    //     </AccordionItem>
+    //   ))}
+    // </Accordion>
     <div className="flex h-60 flex-col items-center justify-center text-slate-400">No Policy</div>
   )
 }
