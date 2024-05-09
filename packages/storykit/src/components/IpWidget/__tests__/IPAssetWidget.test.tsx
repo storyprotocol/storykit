8be257ebf77c201dadb0be244831724d6a8d38cc
samfairbairn <samfairbairn@hotmail.co.uk>
2024-05-09 14:26:09 -0700

    archive storykit

--- packages/storykit/src/components/IpWidget/__tests__/IPAssetWidget.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-
-import IpWidget from "../IpWidget"
-
-describe("IpWidget", () => {
-  test("renders the IpWidget component", () => {
-    render(<IpWidget ipId={"0xbbf08a30b9ff0f717a024a75963d3196aaf0f0dd"} />)
-  })
-})
--- packages/storykit/src/components/IpWidget/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as IpWidget } from "./IpWidget"
