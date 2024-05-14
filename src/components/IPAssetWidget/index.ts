eb173ccb9188904b7838dce418c632ef03a9a0de
samfairbairn <samfairbairn@hotmail.co.uk>
2024-05-14 15:34:27 -0700

    remove merge duplications

--- src/components/IPAssetWidget/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as IPAssetWidget } from "./IPAssetWidget"
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Button"
-export * from "./IPAssetWidget"
-export * from "./IPAGraph"
-export * from "./IPAPolicies"
-export * from "./IPARoyaltyChart"
