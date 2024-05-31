e7ab82a9f68b58a9ac453bf6807daa0684e805cb
Tina-57blocks <tina@57blocks.com>
2024-05-31 17:18:39 +0800

    add test

--- packages/storykit/src/components/IpWidget/__docs__/IpWidget.stories.tsx
+++ packages/storykit/src/components/IpWidget/__docs__/IpWidget.stories.tsx
@@ -1,5 +1,6 @@
 import { PREVIEW_IP_ASSETS } from "@/stories/data"
 import type { Meta, StoryObj } from "@storybook/react"
+import { expect, userEvent, waitFor, within } from "@storybook/test"
 
 import Example from "./Example"
 
@@ -37,5 +38,38 @@ export const Input: Story = {
   args: {
     ipId: PREVIEW_IP_ASSETS[0] as `0x${string}`,
     isBottomNav: true,
+  },
+}
+export const Navigation: Story = {
+  args: {
+    ipId: "0x22Fe8C376919586F344fED952A9448df442b10f2",
+    isBottomNav: false,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await waitFor(
+      () => {
+        expect(canvas.getByText("11155111: Example NFT #4367")).toBeInTheDocument()
+        expect(canvas.getByText("Owned by")).toBeInTheDocument()
+        expect(canvas.getByText("0x3679...12E1")).toBeInTheDocument()
+      },
+      { timeout: 5000 }
+    )
+
+    userEvent.click(canvas.getByText("Licensing"))
+    await waitFor(
+      () => {
+        expect(canvas.getAllByText("Commercial Remix")[0]).toBeInTheDocument()
+      },
+      { timeout: 5000 }
+    )
+
+    await userEvent.click(canvas.getByText("IP Graph"))
+
+    await userEvent.click(canvas.getByText("Royalty"))
+    await waitFor(() => {
+      expect(canvas.getByText("No Royalty Data")).toBeInTheDocument()
+    })
   },
 }
