88aa76768cb46f45e3ae860da54c9afd8fd08c34
Allen Chuang <mr.allenchuang@gmail.com>
2024-06-11 21:35:56 -0700

    fix license options ordering and fix styling

--- packages/storykit/src/components/LicenseTerms/__docs__/LicenseTerms.stories.tsx
+++ packages/storykit/src/components/LicenseTerms/__docs__/LicenseTerms.stories.tsx
@@ -8,13 +8,18 @@ const meta = {
   component: Example,
   parameters: {
     layout: "centered",
+    controls: { sort: "requiredFirst" },
   },
   argTypes: {
     type: {
       options: Object.values(POLICY_TYPE),
     },
   },
-  args: {},
+  args: {
+    direction: "column",
+    size: "medium",
+    type: POLICY_TYPE.COMMERCIAL_REMIX as PolicyType,
+  },
 } satisfies Meta<typeof Example>
 
 export default meta
@@ -22,9 +27,4 @@ type Story = StoryObj<typeof meta>
 
 export const Select: Story = {
   argTypes: {},
-  args: {
-    size: "medium",
-    type: POLICY_TYPE.COMMERCIAL_REMIX as PolicyType,
-    direction: "column",
-  },
 }
