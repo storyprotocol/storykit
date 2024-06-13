f0a7725b23f822e650cc9280d92ba2f2a65ff109
samfairbairn <samfairbairn@hotmail.co.uk>
2024-06-13 14:35:33 +0800

    example-data prettier

--- packages/storykit/src/lib/example-data.ts
+++ packages/storykit/src/lib/example-data.ts
@@ -1,38 +1,38 @@
-import { PILTerms } from "@/types";
-import { zeroAddress } from "viem";
+import { PILTerms } from "@/types"
+import { zeroAddress } from "viem"
 
 export const nonCommercialSocialRemixingLicenseTerms: PILTerms = {
-    // transferable: true,
-    // royaltyPolicy: zeroAddress,
-    // mintingFee: 0,
-    // commercializerCheckData: 
-    commercialAttribution: false,
-    commercialRevenueCelling: 0,
-    commercialRevenueShare: 0,
-    commercialUse: false,
-    commercializerCheck: zeroAddress,
-    currency: zeroAddress,
-    derivativesAllowed: true,
-    derivativesApproval: false,
-    derivativesAttribution: true,
-    derivativesReciprocal: true,
-    derivativesRevenueCelling: 0,
-    expiration: '0',
-    uRI: ''
+  // transferable: true,
+  // royaltyPolicy: zeroAddress,
+  // mintingFee: 0,
+  // commercializerCheckData:
+  commercialAttribution: false,
+  commercialRevenueCelling: 0,
+  commercialRevenueShare: 0,
+  commercialUse: false,
+  commercializerCheck: zeroAddress,
+  currency: zeroAddress,
+  derivativesAllowed: true,
+  derivativesApproval: false,
+  derivativesAttribution: true,
+  derivativesReciprocal: true,
+  derivativesRevenueCelling: 0,
+  expiration: "0",
+  uRI: "",
 }
 
 export const noLicenseTerms: PILTerms = {
-    commercialAttribution: false,
-    commercialRevenueCelling: 0,
-    commercialRevenueShare: 0,
-    commercialUse: false,
-    commercializerCheck: '0x',
-    currency: '0x',
-    derivativesAllowed: false,
-    derivativesApproval: false,
-    derivativesAttribution: false,
-    derivativesReciprocal: false,
-    derivativesRevenueCelling: 0,
-    expiration: '',
-    uRI: ''
+  commercialAttribution: false,
+  commercialRevenueCelling: 0,
+  commercialRevenueShare: 0,
+  commercialUse: false,
+  commercializerCheck: "0x",
+  currency: "0x",
+  derivativesAllowed: false,
+  derivativesApproval: false,
+  derivativesAttribution: false,
+  derivativesReciprocal: false,
+  derivativesRevenueCelling: 0,
+  expiration: "",
+  uRI: "",
 }
