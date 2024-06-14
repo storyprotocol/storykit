3d0ab77e59e91335ff301b804e0d83012f25e7db
samfairbairn <samfairbairn@hotmail.co.uk>
2024-06-14 17:50:31 +0800

    update commercial remix & use example data

--- packages/storykit/src/lib/example-data.ts
+++ packages/storykit/src/lib/example-data.ts
@@ -4,6 +4,22 @@ import { zeroAddress } from "viem"
 export const commercialRemixingLicenseTerms: PILTerms = {
   commercialAttribution: true,
   commercialRevenueCelling: 0,
+  commercialRevenueShare: 10000000,
+  commercialUse: true,
+  commercializerCheck: "0x0000000000000000000000000000000000000000",
+  currency: "0xb132a6b7ae652c974ee1557a3521d53d18f6739f",
+  derivativesAllowed: true,
+  derivativesApproval: false,
+  derivativesAttribution: true,
+  derivativesReciprocal: true,
+  derivativesRevenueCelling: 0,
+  expiration: "never",
+  uRI: "",
+}
+
+export const commercialUseLicenseTerms: PILTerms = {
+  commercialAttribution: true,
+  commercialRevenueCelling: 0,
   commercialRevenueShare: 0,
   commercialUse: true,
   commercializerCheck: "0x0000000000000000000000000000000000000000",
@@ -16,22 +32,6 @@ export const commercialRemixingLicenseTerms: PILTerms = {
   expiration: "never",
   uRI: "",
 }
-
-// export const commercialUseLicenseTerms: PILTerms = {
-//   commercialAttribution: true,
-//   commercialRevenueCelling: 0,
-//   commercialRevenueShare: 1000,
-//   commercialUse: true,
-//   commercializerCheck: "0x0000000000000000000000000000000000000000",
-//   currency: "0xb132a6b7ae652c974ee1557a3521d53d18f6739f",
-//   derivativesAllowed: false,
-//   derivativesApproval: false,
-//   derivativesAttribution: false,
-//   derivativesReciprocal: false,
-//   derivativesRevenueCelling: 0,
-//   expiration: "never",
-//   uRI: "",
-// }
 
 export const nonCommercialSocialRemixingLicenseTerms: PILTerms = {
   // transferable: true,
