fad97b7be58a953c6434c2403de21df2f8c336df
samfairbairn <samfairbairn@hotmail.co.uk>
2024-09-27 16:04:40 +0800

    adding chain option to IpLicenseAccordion

--- packages/storykit/src/components/IpLicenseAccordion/__docs__/Example.tsx
+++ packages/storykit/src/components/IpLicenseAccordion/__docs__/Example.tsx
@@ -1,3 +1,4 @@
+import { STORYKIT_SUPPORTED_CHAIN } from "@/lib/constants"
 import { PREVIEW_IP_ASSETS } from "@/stories/data"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React, { FC } from "react"
@@ -8,15 +9,16 @@ import IpLicenseAccordion from "../IpLicenseAccordion"
 
 type Size = "small" | "medium" | "large"
 
-const Example: FC<{ ipId: Address; size: Size }> = ({
+const Example: FC<{ ipId: Address; chain?: STORYKIT_SUPPORTED_CHAIN; size: Size }> = ({
   ipId = PREVIEW_IP_ASSETS[0] as `0x${string}`,
+  chain = STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
   size = "medium",
 }) => {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
       <div className="flex h-full w-full items-center justify-center">
-        <IpProvider ipId={ipId}>
+        <IpProvider chain={chain} ipId={ipId}>
           <IpLicenseAccordion size={size} />
         </IpProvider>
       </div>
