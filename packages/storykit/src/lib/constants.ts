1316e42354fd4f91c624b9a64cff40109f77aaf1
samfairbairn <samfairbairn@hotmail.co.uk>
2024-10-30 17:39:27 +0800

    get simplehash id from chain

--- packages/storykit/src/lib/constants.ts
+++ packages/storykit/src/lib/constants.ts
@@ -12,3 +12,8 @@ export const CHAINID_TO_CHAINNAME: { [key: number]: string } = {
   1513: "story-testnet",
   1516: "odyssey-testnet",
 }
+
+export const CHAINNAME_TO_SIMPLEHASH_ID: { [key: string]: string } = {
+  "story-testnet": "story-testnet",
+  "odyssey-testnet": "story-odyssey",
+}
