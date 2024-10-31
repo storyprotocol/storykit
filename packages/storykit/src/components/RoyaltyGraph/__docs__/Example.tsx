b241cfbb08dfb3ca260d896c4c44ed2acf4aec83
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-01 01:34:17 +0800

    remove providers from royalty graph

--- packages/storykit/src/components/RoyaltyGraph/__docs__/Example.tsx
+++ packages/storykit/src/components/RoyaltyGraph/__docs__/Example.tsx
@@ -1,12 +1,12 @@
-import { CHAINS, STORYKIT_SUPPORTED_CHAIN } from "@/lib/chains"
+import { STORYKIT_SUPPORTED_CHAIN } from "@/lib/chains"
 import { getCollectionByAddress, getNFTByWallet } from "@/lib/simplehash"
+import { useStoryKitContext } from "@/providers"
 import { RoyaltyGraphProvider } from "@/providers/RoyaltyGraphProvider/RoyaltyGraphProvider"
 import {
   // ILIAD_PREVIEW_IP_ASSETS,
   ILIAD_TESTNET_ROYALTY_GRAPH_IP_ASSETS, // ILIAD_TESTNET_ROYALTY_GRAPH_LAP_ASSETS,
   // ILIAD_TESTNET_ROYALTY_GRAPH_LRP_ASSETS,
 } from "@/stories/data"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React, { FC, useEffect, useState } from "react"
 import { Address } from "viem"
 
@@ -19,42 +19,39 @@ const Example: FC<{
   height?: number
   darkMode?: boolean
   isAnimated?: boolean
-  chain?: STORYKIT_SUPPORTED_CHAIN
 }> = ({
   ipIds = [ILIAD_TESTNET_ROYALTY_GRAPH_IP_ASSETS[0]] as Address[],
   width = 400,
   height = 300,
   darkMode = false,
   isAnimated = false,
-  chain = STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
 }) => {
+  const { chain } = useStoryKitContext()
   const [collections, setCollections] = useState<any>(null)
   const [nfts, setNfts] = useState<any>(null)
 
   useEffect(() => {
     const fetch = async () => {
-      const collectionMetadata = await getCollectionByAddress("0x7ee32b8B515dEE0Ba2F25f612A04a731eEc24F49", chain)
+      const collectionMetadata = await getCollectionByAddress(
+        "0x7ee32b8B515dEE0Ba2F25f612A04a731eEc24F49",
+        chain.name as STORYKIT_SUPPORTED_CHAIN
+      )
       setCollections(collectionMetadata)
-      const nftWalletResponse = await getNFTByWallet("0xB1918E7d6CB67d027F6aBc66DC3273D6ECAD6dE5", chain)
+      const nftWalletResponse = await getNFTByWallet(
+        "0xB1918E7d6CB67d027F6aBc66DC3273D6ECAD6dE5",
+        chain.name as STORYKIT_SUPPORTED_CHAIN
+      )
       setNfts(nftWalletResponse)
     }
     fetch()
   }, [chain])
 
-  const queryClient = new QueryClient()
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <div className="flex h-full items-center justify-center">
-          {/* <IpProvider ipId={ipId} options={{ royaltyGraphData: true }}> */}
-          <RoyaltyGraphProvider ipIds={ipIds}>
-            {/* <div style={{ width: "100%", height: "500px" }}> */}
-            <RoyaltyGraph width={width} height={height} darkMode={darkMode} isAnimated={isAnimated} />
-            {/* </div> */}
-          </RoyaltyGraphProvider>
-          {/* </IpProvider> */}
-        </div>
-      </QueryClientProvider>
+      <RoyaltyGraphProvider ipIds={ipIds}>
+        <RoyaltyGraph width={width} height={height} darkMode={darkMode} isAnimated={isAnimated} />
+      </RoyaltyGraphProvider>
+
       {collections && (
         <div id="collections" style={{ visibility: "hidden", position: "fixed" }}>
           {JSON.stringify(collections)}
