13490a267c949a804ce2041caa9ef3230db52d1c
samfairbairn <samfairbairn@hotmail.co.uk>
2024-10-31 18:01:03 +0800

    pass chain to getResource/listResource functions, use api version from CHAINS constants

--- packages/storykit/src/lib/api.ts
+++ packages/storykit/src/lib/api.ts
@@ -1,5 +1,5 @@
 import { QueryOptions, ResourceType } from "../types/api"
-import { STORYKIT_SUPPORTED_CHAIN } from "./chains"
+import { CHAINS, STORYKIT_SUPPORTED_CHAIN } from "./chains"
 
 const API_URL = process.env.STORYBOOK_API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL
 
@@ -9,14 +9,20 @@ const API_KEY =
   process.env.STORY_PROTOCOL_X_API_KEY ||
   ""
 
-export async function getResource<T>(resourceName: ResourceType, resourceId: string, options?: QueryOptions) {
+export async function getResource<T>(
+  resourceName: ResourceType,
+  resourceId: string,
+  chain: STORYKIT_SUPPORTED_CHAIN,
+  options?: QueryOptions
+) {
   try {
-    const res = await fetch(`${API_URL}/${resourceName}/${resourceId}`, {
+    const _chain = CHAINS[chain]
+    const res = await fetch(`${API_URL}/${_chain.apiVersion}/${resourceName}/${resourceId}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
         "x-api-key": API_KEY as string,
-        "X-CHAIN": options?.chain || STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
+        "X-CHAIN": options?.chain || _chain.name || STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
         "x-extend-asset": "true",
       },
     })
@@ -28,14 +34,19 @@ export async function getResource<T>(resourceName: ResourceType, resourceId: str
   }
 }
 
-export async function listResource<T>(resourceName: ResourceType, options?: QueryOptions) {
+export async function listResource<T>(
+  resourceName: ResourceType,
+  chain: STORYKIT_SUPPORTED_CHAIN,
+  options?: QueryOptions
+) {
   try {
-    const res = await fetch(`${API_URL}/${resourceName}`, {
+    const _chain = CHAINS[chain]
+    const res = await fetch(`${API_URL}/${_chain.apiVersion}/${resourceName}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "x-api-key": API_KEY as string,
-        "X-CHAIN": options?.chain || STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
+        "X-CHAIN": options?.chain || _chain.name || STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
       },
       cache: "no-cache",
       ...(options && { body: JSON.stringify({ options }) }),
