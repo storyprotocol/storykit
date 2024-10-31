fd1dc0e05fb2e2c3d8484adc8b8a3cf7d4ae5752
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-01 01:15:34 +0800

    remove providers from ip royalty pie chart

--- packages/storykit/src/components/IpRoyaltyPieChart/__docs__/Example.tsx
+++ packages/storykit/src/components/IpRoyaltyPieChart/__docs__/Example.tsx
@@ -1,28 +1,17 @@
-import { STORYKIT_SUPPORTED_CHAIN } from "@/lib"
-import { StoryKitProvider } from "@/providers/StoryKitProvider"
 import { ILIAD_PREVIEW_IP_ASSETS } from "@/stories/data"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React, { FC } from "react"
 import { Address } from "viem"
 
 import { IpProvider } from "../../../providers"
 import IpRoyaltyPieChart from "../IpRoyaltyPieChart"
 
-const Example: FC<{ ipId: Address; chain: STORYKIT_SUPPORTED_CHAIN }> = ({
-  ipId = ILIAD_PREVIEW_IP_ASSETS[1] as `0x${string}`,
-  chain = STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
-}) => {
-  const queryClient = new QueryClient()
+const Example: FC<{ ipId: Address }> = ({ ipId = ILIAD_PREVIEW_IP_ASSETS[1] as `0x${string}` }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <StoryKitProvider chain={chain}>
-        <div className="flex h-full items-center justify-center">
-          <IpProvider ipId={ipId}>
-            <IpRoyaltyPieChart />
-          </IpProvider>
-        </div>
-      </StoryKitProvider>
-    </QueryClientProvider>
+    <div className="flex h-full items-center justify-center">
+      <IpProvider ipId={ipId}>
+        <IpRoyaltyPieChart />
+      </IpProvider>
+    </div>
   )
 }
 
