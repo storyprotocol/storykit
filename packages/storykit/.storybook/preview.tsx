d42c154f069322beb3fb2c4179c2c45d2ea23ae8
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-01 01:03:54 +0800

    remove storybook theme addon

--- packages/storykit/.storybook/preview.tsx
+++ packages/storykit/.storybook/preview.tsx
@@ -1,12 +1,9 @@
-import { withThemeByClassName } from "@storybook/addon-themes"
 import type { Preview } from "@storybook/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import { useEffect } from "react"
 
 import { STORYKIT_SUPPORTED_CHAIN } from "../src/lib/chains"
-import { Theme } from "../src/providers/StoryKitProvider"
-// import { StoryKitProvider } from "../src/providers/StoryKitProvider"
 import StoryProvider from "./directors/StoryProvider"
 import "./global.css"
 import theme from "./theme"
@@ -53,8 +50,8 @@ const preview: Preview = {
       toolbar: {
         title: "Mode",
         icon: "sun",
-        items: ["system", "light", "dark"],
-        defaultValue: "default",
+        items: ["light", "dark"],
+        defaultValue: "light",
         dynamicTitle: true,
       },
     },
@@ -62,18 +59,7 @@ const preview: Preview = {
   globals: {
     chain: STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
   },
-  // initialGlobals: {
-  //   chain: 'light',
-  // },
   decorators: [
-    // withThemeByClassName({
-    //   themes: {
-    //     light: "",
-    //     dark: "dark",
-    //   },
-    //   defaultTheme: "light",
-    // }),
-
     (Story, context) => {
       useEffect(() => {
         document.querySelector("html")?.setAttribute("class", context.globals.mode)
