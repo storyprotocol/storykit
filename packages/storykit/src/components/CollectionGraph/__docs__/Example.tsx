cb407c3f4c0db1c6a263cf3d9004c9d67056dc55
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-01 01:10:39 +0800

    remove providers from collection graph example

--- packages/storykit/src/components/CollectionGraph/__docs__/Example.tsx
+++ packages/storykit/src/components/CollectionGraph/__docs__/Example.tsx
@@ -1,52 +1,52 @@
-import { CHAINS, STORYKIT_SUPPORTED_CHAIN } from "@/lib/chains"
+import { STORYKIT_SUPPORTED_CHAIN } from "@/lib/chains"
 import { getCollectionByAddress, getNFTByWallet } from "@/lib/simplehash"
-import { StoryKitProvider } from "@/providers/StoryKitProvider"
+import { useStoryKitContext } from "@/providers"
 import { ILIAD_PREVIEW_COLLECTION_ADDRESS } from "@/stories/data"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React, { FC, useEffect, useState } from "react"
 import { Address } from "viem"
 
 import CollectionGraph, { CollectionGraphProps } from "../CollectionGraph"
 
-const Example: FC<CollectionGraphProps & { chain: STORYKIT_SUPPORTED_CHAIN }> = ({
+const Example: FC<CollectionGraphProps> = ({
   collectionAddress = ILIAD_PREVIEW_COLLECTION_ADDRESS[0] as Address,
   width = 2000,
   height = 1000,
   showName = false,
   showRelationship = false,
   darkMode = false,
-  chain = STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET,
 }) => {
+  const { chain } = useStoryKitContext()
   const [collections, setCollections] = useState<any>(null)
   const [nfts, setNfts] = useState<any>(null)
 
   useEffect(() => {
     const fetch = async () => {
-      const collectionMetadata = await getCollectionByAddress("0x7ee32b8B515dEE0Ba2F25f612A04a731eEc24F49", chain)
+      const collectionMetadata = await getCollectionByAddress(
+        "0x7ee32b8B515dEE0Ba2F25f612A04a731eEc24F49",
+        chain.name as STORYKIT_SUPPORTED_CHAIN
+      )
       setCollections(collectionMetadata)
-      const nftWalletResponse = await getNFTByWallet("0xB1918E7d6CB67d027F6aBc66DC3273D6ECAD6dE5", chain)
+      const nftWalletResponse = await getNFTByWallet(
+        "0xB1918E7d6CB67d027F6aBc66DC3273D6ECAD6dE5",
+        chain.name as STORYKIT_SUPPORTED_CHAIN
+      )
       setNfts(nftWalletResponse)
     }
     fetch()
   }, [chain])
 
-  const queryClient = new QueryClient()
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <StoryKitProvider chain={chain}>
-          <div className="flex h-full items-center justify-center">
-            <CollectionGraph
-              collectionAddress={collectionAddress}
-              width={width}
-              height={height}
-              showName={showName}
-              showRelationship={showRelationship}
-              darkMode={darkMode}
-            />
-          </div>
-        </StoryKitProvider>
-      </QueryClientProvider>
+      <div className="flex h-full items-center justify-center">
+        <CollectionGraph
+          collectionAddress={collectionAddress}
+          width={width}
+          height={height}
+          showName={showName}
+          showRelationship={showRelationship}
+          darkMode={darkMode}
+        />
+      </div>
       {collections && (
         <div id="collections" style={{ visibility: "hidden", position: "fixed" }}>
           {JSON.stringify(collections)}
