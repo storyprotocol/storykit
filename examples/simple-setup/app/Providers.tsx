e6c9da496fabe01b4032dc3f112fb77b1a4f8e52
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-01 16:08:08 +0800

    update basic example

---
+++ examples/simple-setup/app/Providers.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { StoryKitProvider, STORYKIT_SUPPORTED_CHAIN } from "@storyprotocol/storykit"
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StoryKitProvider chain={STORYKIT_SUPPORTED_CHAIN.STORY_TESTNET}>
+        {children}
+      </StoryKitProvider>
+    </QueryClientProvider>
+  )
+}
---
+++ examples/simple-setup/app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { IpProvider, IpLicenseAccordion } from "@storyprotocol/storykit"
+import IpHeader from "./IpHeader"
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen ">
+      <div className="flex w-96 max-w-full">
+        <IpProvider ipId="0xF86cD6796eb8C75eDb5939F9875aaD137aEE4372">
+          <div className="flex flex-col gap-4">
+            <IpHeader />
+            <IpLicenseAccordion />
+        </div>
+        </IpProvider>
+      </div>
+    </div>
+
+  )
+}
