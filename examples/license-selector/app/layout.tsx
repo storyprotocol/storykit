cb59e865d92e7c5b8f169aa2df21d0af358866b0
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-11 20:12:47 +0800

    add license selector example

---
+++ examples/license-selector/app/layout.tsx
@@ -0,0 +1,19 @@
+
+import Providers from "./Providers"
+import "./base.css"
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
