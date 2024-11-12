bad9e5fb57235f71253181ad279841670d607983
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-12 13:28:22 +0800

    update IpWidget stories ipid controls

--- packages/storykit/src/components/IpWidget/__docs__/Example.tsx
+++ packages/storykit/src/components/IpWidget/__docs__/Example.tsx
@@ -1,11 +1,11 @@
-import { ILIAD_PREVIEW_IP_ASSETS } from "@/stories/data"
+import { STORY_IP_ASSETS, STORY_IP_ASSETS_MAP } from "@/stories/data"
 import React, { FC } from "react"
 import { Address } from "viem"
 
 import IpWidget from "../IpWidget"
 
 const Example: FC<{ ipId: Address; isBottomNav?: boolean }> = ({
-  ipId = ILIAD_PREVIEW_IP_ASSETS[1] as "0x${string}",
+  ipId = STORY_IP_ASSETS_MAP[STORY_IP_ASSETS[1]] as "0x${string}",
   isBottomNav = false,
 }) => {
   return <IpWidget ipId={ipId} isBottomNav={isBottomNav} />
