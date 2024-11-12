c3795107f74cf4804fb3c49c65b27d1bd2458057
samfairbairn <samfairbairn@hotmail.co.uk>
2024-11-12 13:11:44 +0800

    export ipid control readable names and mapping

--- packages/storykit/src/stories/data/index.ts
+++ packages/storykit/src/stories/data/index.ts
@@ -1,8 +1,17 @@
 import * as Iliad from "./iliad"
 import * as Odyssey from "./odyssey"
 
+const STORY_ODYSSEY_IP_ASSETS_NAME = Odyssey.ODYSSEY_PREVIEW_IP_ASSETS.map((ip) => `Odyssey: ${ip}`)
+const STORY_ODYSSEY_IP_ASSETS_MAP = Object.fromEntries(
+  Odyssey.ODYSSEY_PREVIEW_IP_ASSETS.map((ip) => [`Odyssey: ${ip}`, ip])
+)
 
+const STORY_ILIAD_IP_ASSETS_NAME = Iliad.ILIAD_PREVIEW_IP_ASSETS.map((ip) => `Iliad: ${ip}`)
+const STORY_ILIAD_IP_ASSETS_MAP = Object.fromEntries(Iliad.ILIAD_PREVIEW_IP_ASSETS.map((ip) => [`Iliad: ${ip}`, ip]))
 
+const STORY_IP_ASSETS = [...STORY_ODYSSEY_IP_ASSETS_NAME, ...STORY_ILIAD_IP_ASSETS_NAME]
+const STORY_IP_ASSETS_MAP = { ...STORY_ODYSSEY_IP_ASSETS_MAP, ...STORY_ILIAD_IP_ASSETS_MAP }
 
 export * from "./iliad"
 export * from "./odyssey"
+export { STORY_IP_ASSETS, STORY_IP_ASSETS_MAP }
