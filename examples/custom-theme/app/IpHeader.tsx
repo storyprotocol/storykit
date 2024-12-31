485bc036dde2cabfa25f745a5721a011d5fe52ce
yuguoqiang <yuguoqiang@hygon.cn>
2024-12-31 18:03:07 +0800

    enhancement: addCustomThemeFileBoundaryProtection

--- examples/custom-theme/app/IpHeader.tsx
+++ examples/custom-theme/app/IpHeader.tsx
@@ -8,7 +8,11 @@ export default function IpHeader() {
       <h2 className="text-xs text-gray-500">By {nftData?.owners[0].owner_address}</h2>
       {nftData?.previews.image_medium_url ? 
         // eslint-disable-next-line @next/next/no-img-element
-        <img src={nftData.previews.image_medium_url} alt={nftData.name} className="w-full aspect-video object-contain border border-gray-200 rounded-md my-2" /> 
+        <img 
+          src={nftData.previews.image_medium_url} 
+          alt={nftData?.name || "No image available"} 
+          className="w-full aspect-video object-contain border border-gray-200 rounded-md my-2" 
+        /> 
       : null}
     </div>
   )
